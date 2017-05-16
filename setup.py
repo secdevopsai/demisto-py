949254c19c4fb449336dd28df6b6235c10c90e9a
asafshen <asaf@demisto.com>
2017-05-16 19:58:04 +0300

    setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Demisto-python',
+      version='1.0',
+      description='Demisto Client for python',
+      author='Slavik',
+      url='https://www.python.org/sigs/distutils-sig/',
+      packages=[''],
+     )
